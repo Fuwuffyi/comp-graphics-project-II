650dcd7e570787e625b43f4989736ef477d56220
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2024-12-01 16:47:59 +0100

    Remove 'A'

--- ProgettoIICompGraphics/Mesh2D.cpp
+++ ProgettoIICompGraphics/Mesh2D.cpp
@@ -1,8 +1,6 @@
 #include "Mesh2D.hpp"
 #include "Vertex.hpp"
 
-#include <iostream>
-
 #include <glad/glad.h>
 
 Mesh2D::Mesh2D(const std::vector<Vertex2D>& vertices, const std::vector<uint32_t>& indices, const uint32_t _drawType) 
@@ -20,7 +18,6 @@ Mesh2D::Mesh2D(const std::vector<Vertex2D>& vertices, const std::vector<uint32_t
 
 void Mesh2D::setVertexArrayAttributes() const {
 	// Link the vertice's attributes to slots: (0 = vec2 position, 1 = vec2 normal, 2 = vec2 uv)
-	std::cout << "A" << std::endl;
 	this->vao.linkAttrib(0, 2, sizeof(Vertex2D), GL_FLOAT, 0);
 	this->vao.linkAttrib(1, 2, sizeof(Vertex2D), GL_FLOAT, 2 * sizeof(float));
 	this->vao.linkAttrib(2, 2, sizeof(Vertex2D), GL_FLOAT, 4 * sizeof(float));
