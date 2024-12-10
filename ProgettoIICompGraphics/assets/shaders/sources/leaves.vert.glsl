6493f396ceccba9efef486fdf1d44ceb6d79499e
Fuwuffyi <luca.palazzini3@studio.unibo.it>
2024-12-10 21:46:46 +0100

    Add moving leaves

--- ProgettoIICompGraphics/assets/shaders/sources/leaves.vert.glsl
+++ ProgettoIICompGraphics/assets/shaders/sources/leaves.vert.glsl
@@ -11,16 +11,19 @@ out vec2 uvIn;
 out vec3 worldPosition;
 out mat3 TBN;
 
+uniform float glfwTime;
 uniform mat4 objMatrix;
 uniform mat4 cameraMatrix;
+uniform vec3 material_windDirection;
+uniform float material_windStrength;
 
 void main() {
     worldPosition = vec3(objMatrix * vec4(aPos, 1.0));
+    worldPosition += material_windDirection * sin(glfwTime + 0.5 * length(worldPosition * material_windDirection)) * material_windStrength;
     gl_Position = cameraMatrix * vec4(worldPosition, 1.0);
     mat3 normalMatrix = transpose(inverse(mat3(objMatrix)));
     normalIn = normalize(normalMatrix * aNormal);
     uvIn = aUv;
-
     vec3 tangent = normalize(mat3(objMatrix) * aTangent);
     vec3 bitangent = normalize(mat3(objMatrix) * aBitangent);
     TBN = mat3(tangent, bitangent, normalIn);
